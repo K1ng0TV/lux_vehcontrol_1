ff0178f0a89324a4f159ea7b2f8ead95f95f2b1c
K1ng0TV <152930830+K1ng0TV@users.noreply.github.com>
2024-08-03 17:25:41 +0200

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+description 'lux_vehcontrol'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+shared_script '@ox_lib/init.lua'
